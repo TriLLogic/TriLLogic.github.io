e8d0c439b767dedafe4be6918cc4b5d7f6c2c97f
Michael Ryan <mehmenmike@users.noreply.github.com>
2015-07-16 09:45:00 +0100

    Update name.js

--- name.js
+++ name.js
@@ -30,5 +30,5 @@ function checkCookie() {
     }
 }
 var n = getCookie("username")
-document.getElementById('.results').innerHtml = n;
+document.querySelector('.results').innerHTML = n;
 //document.getElementById("output").innerHTML=name;
