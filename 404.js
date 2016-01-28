97e2e8ffee357d881486692efac5d1ff039ab265
Michael Ryan <mehmenmike@users.noreply.github.com>
2016-01-28 16:49:47 +0000

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,5 +1,5 @@
 function redirect(){
-  links = ["http://thebest404pageever.com/swf/Carlton.swf", "http://thebest404pageever.com/swf/Super_Spice_Bros_64.swf", "http://thebest404pageever.com/swf/Spinnarkura.swf", "http://thebest404pageever.com/swf/TERRORISTS_WIN_AGAIN.swf", "http://thebest404pageever.com/swf/scientific_fus_ro_dah.swf", "http://thebest404pageever.com/swf/TrollingSaruman.swf"];
+  links = ["http://thebest404pageever.com/swf/Carlton.swf", "http://thebest404pageever.com/swf/Super_Spice_Bros_64.swf", "http://thebest404pageever.com/swf/Spinnarkura.swf", "http://thebest404pageever.com/swf/TERRORISTS_WIN_AGAIN.swf", "http://thebest404pageever.com/swf/scientific_fus_ro_dah.swf", "http://thebest404pageever.com/swf/TrollingSaruman.swf", "http://thebest404pageever.com/swf/negru.swf", "http://thebest404pageever.com/swf/bupbup.swf", "http://thebest404pageever.com/swf/GOD_DAMN_ITS_GOOD_TO_BE_FREE.swf"];
   window.location.href = links[getRandomInt(0, links.length)];
 }
 
